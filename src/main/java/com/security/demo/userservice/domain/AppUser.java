faba9c72ae495c589f8cf9b18df1da587f2760e2
mertoatakan <mertoatakan@gmail.com>
2023-02-17 17:48:32 +0300

    Initial commit(AppUser and Role classes are added)

---
+++ src/main/java/com/security/demo/userservice/domain/AppUser.java
@@ -0,0 +1,8 @@
+package com.security.demo.userservice.domain;
+
+public class AppUser {
+    private Long id;
+    private String name;
+    private String username;
+    
+}
---
+++ src/main/java/com/security/demo/userservice/domain/Role.java
@@ -0,0 +1,4 @@
+package com.security.demo.userservice.domain;
+
+public class Role {
+}
