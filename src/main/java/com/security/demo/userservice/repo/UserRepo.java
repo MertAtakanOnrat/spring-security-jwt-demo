2d5585d3138d2d5fa150da814f32604631524f57
mertoatakan <mertoatakan@gmail.com>
2023-02-17 17:57:32 +0300

    UserRepo ,which extends JpaRepository, is added

---
+++ src/main/java/com/security/demo/userservice/repo/UserRepo.java
@@ -0,0 +1,7 @@
+package com.security.demo.userservice.repo;
+
+import com.security.demo.userservice.domain.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<AppUser, Long> {
+}
